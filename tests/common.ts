fd3bd5bffa8b711a6688dd661c9e509245259112
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2024-10-09 12:37:10 +0200

    adapt tests to new payment page

--- tests/common.ts
+++ tests/common.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { delay, randomize } from "./utils";
+import { delay, randomize, clickWithRetry } from "./utils";
 import { PaymentTypes } from "./types";
 
 export const goToProduct = async (page, sku = 'regular-product') => {
@@ -68,39 +68,44 @@ export const goThroughPaymentPage = async ({
   await test.step(`easyCredit-Ratenkauf Payment`, async () => {
     await page.getByTestId("uc-deny-all-button").click();
 
-	await expect(
+    await expect(
       page.getByText(
         paymentType === PaymentTypes.INSTALLMENT
-          ? "Ihre monatliche Wunschrate"
-          : "Rechnung"
+          ? "Monatliche Wunschrate"
+          : "Ihre Bezahloptionen"
       )
     ).toBeVisible();
 
     await page.getByRole("button", { name: "Weiter zur Dateneingabe" }).click();
 
     if (express) {
-      await page.locator("#vorname").fill(randomize("Ralf"));
-      await page.locator("#nachname").fill("Ratenkauf");
+      await page.locator("#firstName").fill(randomize("Ralf"));
+      await page.locator("#lastName").fill("Ratenkauf");
     }
 
-    await page.locator("#geburtsdatum").fill("05.04.1972");
+    await page.locator("#dateOfBirth").fill("05.04.1972");
 
     if (express) {
-      await page.locator("#email").fill("ralf.ratenkauf@teambank.de");
+      await page.locator("#email").getByRole('textbox').fill("ralf.ratenkauf@teambank.de");
     }
-    await page.locator("#mobilfunknummer").fill("015112345678");
-    await page.locator("#iban").fill("DE12500105170648489890");
+
+    await page.locator("tbk-vorwahldropdown .tel-wrapper").click();
+    await page.locator('tbk-vorwahldropdown').locator("p").filter({ hasText: "+49" }).click();
+    await page.locator('#mobilfunknummer').getByRole('textbox').fill('1703404848');
+    await page.locator('app-ratenkauf-iban-input-dumb').getByRole('textbox').fill("DE12500105170648489890");
 
     if (express) {
-      await page.locator("#strasseHausNr").fill("Beuthener Str. 25");
-      await page.locator("#plz").fill("90402");
-      await page.locator("#ort").fill("Nürnberg");
+      await page.locator("#streetAndNumber").fill("Beuthener Str. 25");
+      await page.locator("#postalCode").fill("90402");
+      await page.locator("#city").fill("Nürnberg");
     }
 
-    await page.getByText("Allen zustimmen").click();
+    await page.locator("#agreeAll").click();
 
     await delay(500);
-    await page.getByRole("button", { name: "Ratenwunsch prüfen" }).click();
+    await clickWithRetry(
+      page.getByRole("button", { name: "Zahlungswunsch prüfen" })
+    );
 
     await delay(500);
     await page.getByRole("button", { name: "Ratenwunsch übernehmen" }).click();
--- tests/utils.ts
+++ tests/utils.ts
@@ -39,3 +39,19 @@ export const minutes = (min: number) => {
 export const seconds = (sec: number) => {
   return sec * 1000;
 };
+
+export async function clickWithRetry(locator, maxRetries = 3) {
+  let attempt = 0;
+  while (attempt < maxRetries) {
+    try {
+      await locator.click();
+      return;
+    } catch (e) {
+      console.error(`Click failed on attempt ${attempt + 1}: ${e.message}`);
+      attempt++;
+      if (attempt === maxRetries) {
+        throw new Error(`Max retries reached, click failed: ${e.message}`);
+      }
+    }
+  }
+}
